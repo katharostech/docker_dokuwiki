6b904c40862a7773825a78b7ed944f7da4f9baeb
Zicklag <zicklag@katharostech.com>
2017-07-13 19:40:36 -0500

    Added proper logging to the container
    Also switched the line ending to Unix style on the php-fpm config.

--- start-container.sh
+++ start-container.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
 # Start php-fpm
+echo "Starting php-fpm"
 php-fpm7.0
 
 # Start nginx
+echo "Starting nginx"
 nginx
+
+nginx_error_log=/var/log/nginx/error.log
+nginx_access_log=/var/log/nginx/access.log
+php_log=/var/log/php7.0-fpm.log
+
+# Tail the logs, after making sure they esist, for docker logs
+while [ ! -f $nginx_error_log ]; do :; done
+while [ ! -f $nginx_access_log ]; do :; done
+while [ ! -f $php_log ]; do :; done
+tail -f $nginx_error_log $nginx_access_log $php_log &
